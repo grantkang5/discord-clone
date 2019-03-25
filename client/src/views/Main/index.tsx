cfe92b79624e6b1020c0ce2ea060584aff42e53c
grantkang5 <grantkang5@gmail.com>
2019-03-25 11:45:48 -0700

    Removes tooltip causing issues with hooks

--- client/src/views/Main/index.tsx
+++ client/src/views/Main/index.tsx
@@ -1,15 +1,10 @@
 import React, { Suspense } from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
-import ChannelsProvider from '../../services/ChannelsProvider'
 import Channels from '../Channels'
 import { Loading } from '../../components/Loaders'
 
 const Main = () => {
-  return (
-    <ChannelsProvider>
-      <Channels />
-    </ChannelsProvider>
-  )
+  return <Channels />
 }
 
 export default Main
