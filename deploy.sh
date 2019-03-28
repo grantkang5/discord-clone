0b4213a346b76a7eb69b302325109504dc3711fd
grantkang5 <grantkang5@gmail.com>
2019-03-28 00:44:44 -0700

    added deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,16 @@
-docker build -t grant05/discord-clone-client -f ./client/Dockerfile ./client
+docker build -t grant05/discord-clone-client:latest -t grant05/discord-clone-client:$SHA -f ./client/Dockerfile ./client
+docker build -t grant05/discord-clone-server:latest -t grant05/discord-clone-server:$SHA -f ./server/Dockerfile ./server
+docker build -t grant05/discord-clone-worker:latest -t grant05/discord-clone-worker:$SHA -f ./redis-server/Dockerfile ./redis-server
+
+docker push grant05/discord-clone-client:latest
+docker push grant05/discord-clone-server:latest
+docker push grant05/discord-clone-worker:latest
+
+docker push grant05/discord-clone-client:$SHA
+docker push grant05/discord-clone-server:$SHA
+docker push grant05/discord-clone-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=grant05/discord-clone-server:$SHA
+kubectl set image deployments/client-deployment client=grant05/discord-clone-client:$SHA
+kubectl set image deployments/worker-deployment worker=grant05/discord-clone-worker:$SHA
