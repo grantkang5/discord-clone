6c2397b530ce61c4deca8a5db7b3f147ce9dd838
grantkang5 <grantkang5@gmail.com>
2019-03-29 12:39:55 -0700

    change ormconfig

---
+++ server/ormconfig.ts
@@ -0,0 +1,18 @@
+module.exports = {
+  "type": "postgres",
+  "host": "postgres",
+  "port": 5432,
+  "username": "postgres",
+  "password": "pgpassword",
+  "database": "postgres",
+  "synchronize": process.env.NODE_ENV === 'development' ? true : false,
+  "logging": false,
+  "entities": ["src/entity/**/*.ts"],
+  "migrations": ["src/migration/**/*.ts"],
+  "subscribers": ["src/subscriber/**/*.ts"],
+  "cli": {
+    "entitiesDir": "src/entity",
+    "migrationsDir": "src/migration",
+    "subscribersDir": "src/subscriber"
+  }
+}
