ce3589e4e7c923f659c4f04ab4017158a8c54212
grantkang5 <grantkang5@gmail.com>
2019-03-31 20:11:05 -0700

    change dist path

--- server/ormconfig.ts
+++ server/ormconfig.ts
@@ -8,9 +8,9 @@ module.exports = {
   "synchronize": process.env.NODE_ENV === 'development' ? true : false,
   "logging": false,
   "keepConnectionAlive": true,
-  "entities": ["dist/entity/**/*.ts"],
-  "migrations": ["dist/migration/**/*.ts"],
-  "subscribers": ["dist/subscriber/**/*.ts"],
+  "entities": ["dist/entity/**/*.*"],
+  "migrations": ["dist/migration/**/*.*"],
+  "subscribers": ["dist/subscriber/**/*.*"],
   "cli": {
     "entitiesDir": "src/entity",
     "migrationsDir": "src/migration",
