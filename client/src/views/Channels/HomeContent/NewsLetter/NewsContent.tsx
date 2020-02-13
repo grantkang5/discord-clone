c04a4485084cfe8304c349cc24c3078d0012fc73
grantkang5 <g1k05j@gmail.com>
2020-02-13 13:41:58 -0800

    change front content

--- client/src/views/Channels/HomeContent/NewsLetter/NewsContent.tsx
+++ client/src/views/Channels/HomeContent/NewsLetter/NewsContent.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import moment from 'moment'
+// import moment from 'moment'
 import style from './NewsLetter.module.css'
 
 const NewsContent = ({ commits }) => {
@@ -13,7 +13,16 @@ const NewsContent = ({ commits }) => {
           https://github.com/grantkang5/discord-clone
         </a>
       </span>
-      {commits.map(data => {
+      <span>
+        Hi, welcome to discord-clone. This is a side-project created by me to experiment with
+        React's experimental Suspense feature along with apollo hooks. This is strictly for
+        experimental and personal use and isn't an exact replica of Discord's webapp. 
+        There are lots of features missing so feel free to point them out/contribute and maybe
+        I'll try to implement them in the future. 
+
+        The app is built using React + Apollo on the FE; TypeORM and apollo graphql on top of Express for the BE as well as a redis-server to help maintain subscription/live features.
+      </span>
+      {/* {commits.map(data => {
         return (
           <div key={data.sha} className={style.commit}>
             <time className={style.commitTime}>
@@ -22,7 +31,7 @@ const NewsContent = ({ commits }) => {
             <div className={style.commitMessage}>{data.commit.message}</div>
           </div>
         )
-      })}
+      })} */}
     </div>
   )
 }
